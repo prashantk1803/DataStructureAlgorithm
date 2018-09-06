801d851aac1a922ca08bee3648b2285fc8571aa4
prashantk1803 <39888864+prashantk1803@users.noreply.github.com>
2018-09-06 15:45:05 -0400

    Add two numbers using bitwise operator

---
+++ addtwonumbers.cpp
@@ -0,0 +1,19 @@
+//Add two numbers using bitwise operator
+#include <iostream>
+
+using namespace std;
+#if 0
+int main() {
+
+	int a=4,b=5;
+
+	while(b != 0)
+	{
+		int carry = a & b;
+		
+		a=a^b;
+		b=carry << 1;
+	}
+	cout << "sum = " << a <<endl;
+}
+#endif
